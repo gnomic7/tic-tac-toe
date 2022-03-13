d2ecb2604c10eec44ffed815d22b03d95513c569
rohit <gnomic7@gmail.com>
2022-03-13 15:29:46 -0400

    Add logic for colWin and part of diagWin

--- app/components/Board.tsx
+++ app/components/Board.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { initBoardValue } from '~/utils/init';
 
 type Board = typeof initBoardValue;
@@ -6,19 +6,28 @@ type Board = typeof initBoardValue;
 type PlayerSymbol = 'O' | 'X';
 
 const diagonalWin = (board: Board, sym: PlayerSymbol) => {
-  // board.some((row, idx) =>
-  //   row.every((col) => board[idx][idx] === col && col === sym),
-  // );
-  // Your code goes here -
+  let win = true;
+  const lastIdx = board[0].length - 1;
+  for (let i = 0; i <= lastIdx; i++) {
+    // There is still a bug here
+    win =
+      (win && board[i][i] === sym) || (win && board[lastIdx - i][i] === sym);
+  }
+  return win;
 };
 
 const colWin = (board: Board, sym: PlayerSymbol) => {
-  // Your code goes here
-  // const colWin = board.map((row, idx) =>
-  //   row.map((col, cIdx) => console.log(idx, cIdx)),
-  // );
+  for (let i = 0; i < board[0].length; i++) {
+    let win = true;
+    for (let j = 0; j < board[0].length; j++) {
+      win = win && board[j][i] === sym;
+    }
+    if (win) {
+      return win;
+    }
+  }
+  return false;
 };
-
 // This is kinda ready
 const rowWin = (board: Board, sym: PlayerSymbol) =>
   board.some((row) => row.every((item) => item === sym));
@@ -31,11 +40,6 @@ const isWinner = (board: Board, sym: PlayerSymbol) =>
 const gameOver = (board: Board) =>
   board.every((row) => !row.some((item) => item === '-'));
 
-export const loader = () =>
-  Array(3)
-    .fill('')
-    .map((item) => Array(3).fill('-'));
-
 const Board = ({
   initialBoard,
   initialPlayer,
@@ -44,43 +48,46 @@ const Board = ({
   initialPlayer: 'O' | 'X';
 }) => {
   const [board, setBoard] = React.useState<Board>(initialBoard);
-  console.log(board);
   const [player, setPlayer] = React.useState<PlayerSymbol>(initialPlayer);
 
-  const handleClick = ({ target }: MouseEvent) => {
+  const handleClick = ({ target }: any) => {
     const [x, y] = target?.id.split(',') || [];
     board[+x][+y] = player;
     setBoard(board);
     setPlayer(player === 'O' ? 'X' : 'O');
     if (isWinner(board, player)) {
-      console.log(`Player with ${player} wins!`);
-      setTimeout(() => window.location.reload(), 3000);
+      console.log(board, player);
+      console.log(
+        rowWin(board, player),
+        colWin(board, player),
+        diagonalWin(board, player),
+      );
+      alert(`Player with ${player} wins!`);
+      // window.location.reload();
     }
 
     if (gameOver(board)) {
-      console.log('The game is a tie!', initBoardValue);
-      setTimeout(() => window.location.reload(), 3000);
+      alert('The game is a tie!');
+      window.location.reload();
     }
   };
   return (
     <>
       <div className="flex flex-col mx-auto">
-        {board.map((row, idx) => {
-          return (
-            <div className="flex items-center border-solid" key={idx}>
-              {row.map((cell, cIdx) => (
-                <button
-                  onClick={handleClick}
-                  className="py-12 px-12 inline border-2 hover:inline-offset-2 bg-indigo-500 font-bold hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300"
-                  key={`${idx},${cIdx}`}
-                  id={`${idx},${cIdx}`}
-                >
-                  {cell}
-                </button>
-              ))}
-            </div>
-          );
-        })}
+        {board.map((row, idx) => (
+          <div className="flex items-center border-solid" key={idx}>
+            {row.map((cell, cIdx) => (
+              <button
+                onClick={handleClick}
+                className="py-12 px-12 inline border-2 hover:inline-offset-2 bg-pink-500 font-bold hover:bg-pink-400 active:bg-pink-600 focus:outline-none focus:ring focus:ring-pink-300"
+                key={`${idx},${cIdx}`}
+                id={`${idx},${cIdx}`}
+              >
+                {cell}
+              </button>
+            ))}
+          </div>
+        ))}
       </div>
     </>
   );
